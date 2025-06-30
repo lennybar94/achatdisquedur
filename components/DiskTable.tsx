22a3b55ba9bd75b2e1d92a912606979e84c17492
Lenny Bar <159714097+lennybar94@users.noreply.github.com>
2025-06-30 21:18:11 +0200

    Update DiskTable.tsx

--- components/DiskTable.tsx
+++ components/DiskTable.tsx
@@ -119,8 +119,10 @@ export default function DiskTable() {
           </div></div>
 
           {/* Conditional filter */}
-          <div className="filter-box"><h3>{selectedKind === 'HDD/SSD' ? 'Type de stockage' : 'Interface'}</h3>
-            {(selectedKind === 'HDD/SSD' ? storageTypes : interfaces).map(item => <label key={item}><input type="checkbox"
+          <div className="filter-box"><h3>
+            {(selectedKind === 'HDD/SSD' || selectedKind === 'Carte Mémoire') ? 'Type de stockage' : 'Interface'}
+                                      </h3>
+            {(selectedKind === 'HDD/SSD' || selectedKind === 'Carte Mémoire') ? storageTypes : interfaces).map(item => <label key={item}><input type="checkbox"
               checked={selectedKind === 'HDD/SSD' ? selectedStorage.has(item) : selectedInterface.has(item)}
               onChange={e => { const c = new Set<string>(selectedKind === 'HDD/SSD' ? selectedStorage : selectedInterface); e.target.checked ? c.add(item) : c.delete(item); if (selectedKind === 'HDD/SSD') setSelectedStorage(c); else setSelectedInterface(c); setCurrentPage(1);} } /> {item}</label>)}
           </div>
@@ -153,4 +155,3 @@ export default function DiskTable() {
     </Fragment>
   );
 }
-
