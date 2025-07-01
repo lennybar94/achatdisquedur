1ee67b7edd73852ac4eccf2e3f4b68c5cc8b1c10
Lenny Bar <159714097+lennybar94@users.noreply.github.com>
2025-07-01 21:39:35 +0200

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,15 +3,17 @@ import ThemeToggle from './ThemeToggle';
 
 export default function Header() {
   return (
-    <header className="topnav">
-      <a href="/">Comparatif disques durs</a>
-      <a href="/comment-choisir-disque-dur">Comment choisir son disque dur ?</a>
-      <a href="/a-propos">A propos</a>
-
-      {/* bouton thème collé à droite */}
-      <span style={{ marginLeft: 'auto' }}>
-        <ThemeToggle />
-      </span>
+    <header className="topnav" role="banner">
+      <nav className="container flex items-center" aria-label="Navigation principale">
+        <ul className="flex gap-6 list-none p-0 m-0">
+          <li><a href="/" className="nav-link">Comparatif disques durs</a></li>
+          <li><a href="/comment-choisir-disque-dur" className="nav-link">Comment choisir son disque dur ?</a></li>
+          <li><a href="/a-propos" className="nav-link">À propos</a></li>
+        </ul>
+        <div className="ml-auto">
+          <ThemeToggle />
+        </div>
+      </nav>
     </header>
   );
 }
