1d093d71ebff2ec1867dafc5d1a8b8879879400b
Lenny Bar <159714097+lennybar94@users.noreply.github.com>
2025-07-03 23:37:17 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,9 @@ export default function Home() {
       <p>
         Découvrez en temps réel le top des supports de stockage Amazon 2025 : HDD internes & externes, SSD, clés USB et cartes mémoire.
       </p>
+      <p>
+      Que vous soyez un professionnel à la recherche d’une solution fiable pour vos sauvegardes, un gamer souhaitant des temps de chargement ultra-rapides, ou simplement un utilisateur occasionnel ayant besoin d’espace supplémentaire, notre comparatif passe en revue les marques phares (Seagate, Western Digital, Samsung, Kingston, SanDisk, Toshiba…), les formats (2,5″, 3,5″, USB-C, microSD), ainsi que les interfaces (SATA III, USB 3.2 Gen2, NVMe).
+      </p>
 
       <h3>Tableau des meilleures ventes</h3>
 
